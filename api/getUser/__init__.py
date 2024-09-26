1ab2cfbb4eb9a83453e0bf9e7da8d79f6b7c8bdb
johnShmulsky <john.shmulsky@gptchb.org>
2024-09-26 07:28:43 -0600

    Update __init__.py

--- api/getUser/__init__.py
+++ api/getUser/__init__.py
@@ -44,7 +44,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     clientPrincipal64=headersAsDict.get('x-ms-client-principal','')
     base64_bytes = clientPrincipal64.encode("ascii")
     principal_string_bytes = base64.b64decode(base64_bytes)
-    principal_string = sample_string_bytes.decode("ascii")
+    principal_string = principal_string_bytes.decode("ascii")
     principal = json.loads(principal_string)
     userData = getGraphData(principal['userDetails'])
     principal['displayName']=userData['displayName']
