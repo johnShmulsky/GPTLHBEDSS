706126d324aecab4fadb1d73d33c8f5fb9690b7f
johnShmulsky <john.shmulsky@gptchb.org>
2024-09-30 08:11:14 -0600

    Update Index.jsx

--- src/pages/DataPage/Index.jsx
+++ src/pages/DataPage/Index.jsx
@@ -25,7 +25,7 @@ const demoData = [
     title: "Aspirin",
     subTitle: "Aspirin sub title",
     desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged",
-    img: Img1,
+    img: "../../media/stats.png",
   },
   {
     id: 2,
