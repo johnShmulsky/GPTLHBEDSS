4370d37571bb35f66de0c71f42b07fc8905ae088
johnShmulsky <john.shmulsky@gptchb.org>
2024-10-03 07:32:18 -0600

    Update __init__.py

--- api/getRoles/__init__.py
+++ api/getRoles/__init__.py
@@ -1,27 +1,38 @@
 import json
-import copy
+import aiohttp
 import azure.functions as func
 
 default_roles= {
-  '12345678':'oglala',
-  
+  'access':'97103693-cad6-4409-b44b-fbe4e342e601',
 }
 
+endpoint = 'https://graph.microsoft.com/v1.0/me/memberOf'
 
-def getAccessToken(req):
-  try:
-    claims = req.get_json()
-    accessToken = claims['accessToken']
-    return "pass"
-  except:
-    return "fail"
+async def isUserInGroup(groupId, bearerToken):
+  headers = {
+    'Authorization': 'Bearer {0}'.format(bearerToken)
+  }
+  params = {
+    'filter':'id eq {0}'.format(groupId)
+  }
+  async with aiohttp.ClientSession() as client:
+    async with client.get( endpoint,headers=headers, params=params) as response:
+      if response.status == 200:
+        return False
+      userData = await response.json()   
+      return True
+  return False    
+  
 
-
-def getRoles(req):
+async def getRoles(token):
   roles = ['testrole', 'authenticated', 'anonymous']
-  roles.append(getAccessToken(req))
+  for role, groupId in default_roles.items():
+    if await isUserInGroup(groupId, token):
+      roles.append(role)
   return {"roles":roles}
 
 
-def main(req):
-  return func.HttpResponse(json.dumps(getRoles(req),indent=4))
+async def main(req: func.HttpRequest) -> func.HttpResponse:
+  claims = req.get_json()
+  accessToken = claims['accessToken']
+  return func.HttpResponse(json.dumps(getRoles(accessToken),indent=4))
