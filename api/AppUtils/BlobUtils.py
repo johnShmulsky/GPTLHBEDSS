42e190cb35d0b18df7e2ea9e408c89e79efe4b3f
johnShmulsky <john.shmulsky@gptchb.org>
2024-10-11 14:48:20 -0600

    Create BlobUtils.py

---
+++ api/AppUtils/BlobUtils.py
@@ -0,0 +1,15 @@
+import azure.identity import ClientSecretCredential
+from azure.storage.blob.aio import BlobServiceClient, ContainerClient
+import os
+
+account_url = "https://sagpthdedsstest.blob.core.windows.net"
+tenant_id = "704bef3c-134c-430e-860d-b9b9baa16039"
+client_id = os.environ['AZURE_CLIENT_ID']
+client_secret = os.environ['AZURE_CLIENT_SECRET_APP_SETTING_NAME']
+credential = ClientSecretCredential(tenant_id, client_id, client_secret)
+
+
+
+async def listBlobs(container):
+    with ContainerClient(account_url=account_url,container_name=container,credential=credential) as client:
+        return await client.list_blobs()   
