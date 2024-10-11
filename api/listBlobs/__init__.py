3de938ad9a553cd6e78efa072eb33a2267abc920
johnShmulsky <john.shmulsky@gptchb.org>
2024-10-11 14:58:15 -0600

    Update __init__.py

--- api/listBlobs/__init__.py
+++ api/listBlobs/__init__.py
@@ -5,9 +5,13 @@ import logging
 
 
 async def main(req: func.HttpRequest) -> func.HttpResponse:
-    return_json = []
-    container = req.params.get('container')
-    blobNames = await BlobUtils.listBlobs(container)
-    for blobName in blobNames:
-        return_json.append({'container':container ,'blob':blobName})
-    return func.HttpResponse(json.dumps(return_json,indent=4))
+    try:
+        return_json = []
+        container = req.params.get('container')
+        blobNames = await BlobUtils.listBlobs(container)
+        for blobName in blobNames:
+            return_json.append({'container':container ,'blob':blobName})
+        return func.HttpResponse(json.dumps(return_json,indent=4))
+    except Exception as e:
+        return func.HttpResponse(e.message)
+        
