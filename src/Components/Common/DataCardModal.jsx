27568d78c42dfd3c7f6648e866da4dab90d9d4cb
johnShmulsky <john.shmulsky@gptchb.org>
2024-10-14 13:25:29 -0600

    Update DataCardModal.jsx

--- src/Components/Common/DataCardModal.jsx
+++ src/Components/Common/DataCardModal.jsx
@@ -1,60 +1,41 @@
 /* eslint-disable react/prop-types */
 import {
-  Box,
+  Button,
   Modal,
   ModalBody,
+  ModalCloseButton,
   ModalContent,
+  ModalFooter,
+  ModalHeader,
   ModalOverlay,
 } from "@chakra-ui/react";
 import React from "react";
 import TableauEmbed from "../Tableau/TableauEmbed";
 import useMediaQuery from "../../hooks/useMediaQuery";
-import Img1 from "../../media/stats.png";
-import Img2 from "../../media/img2.png";
-import Img3 from "../../media/img3.png";
-import Img4 from "../../media/img4.png";
-
 
 // eslint-disable-next-line react/prop-types
 const DataCardModal = ({ isOpen, onClose, data }) => {
   const isMobile = useMediaQuery("(max-width: 500px)");
-  const images = {    
-    Img1: Img1,
-    Img2: Img2,
-    Img3: Img3,
-    Img4: Img4,
-  };
+
+  /**
+  TODO: Testing report rendering on server
+  
+  */
+
   return (
     <div>
-      <Modal
-        blockScrollOnMount={true}
-        isOpen={isOpen}
-        onClose={onClose}
-        closeOnOverlayClick={true}
-        scrollBehavior="inside"
-      >
+      <Modal blockScrollOnMount={false} isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
-        { data.type === "tableau" ? (          
-        <ModalContent minW={isMobile ? "100%" : "1000px"} overscrollY="hidden">
-          <ModalBody scrollBehavior="smooth">
-            <Box height={isMobile ? "1550px" : "1300px"}>
-              <TableauEmbed />
-            </Box>
-          </ModalBody>
-        </ModalContent>  ) : (          
-        <ModalContent>
+        <ModalContent minW={isMobile ? "100%" : "1000px"}>
           <ModalHeader bg="#2E72B9" color="white">
             {data.title}
           </ModalHeader>
+
           <ModalCloseButton color="white" />
           <ModalBody>
-            <Image
-              src={images[data.img]}
-              height="100%"
-              width="100%"
-              objectFit="cover"
-            />
+            <TableauEmbed />
           </ModalBody>
+
           <ModalFooter>
             <Button colorScheme="red" mr={3} onClick={onClose}>
               Close
@@ -63,9 +44,10 @@ const DataCardModal = ({ isOpen, onClose, data }) => {
               ok
             </Button>
           </ModalFooter>
-        </ModalContent>) }      
+        </ModalContent>
       </Modal>
     </div>
   );
 };
+
 export default DataCardModal;
