98519374423e826ba0cd283d8875ef7261ba65d9
johnShmulsky <john.shmulsky@gptchb.org>
2024-10-14 13:53:41 -0600

    Update StaticImage.jsx

--- src/Components/StaticImage/StaticImage.jsx
+++ src/Components/StaticImage/StaticImage.jsx
@@ -2,9 +2,18 @@
 import {
   Image,
 } from "@chakra-ui/react";
+import Img1 from "../../media/stats.png";
+import Img2 from "../../media/img2.png";
+import Img3 from "../../media/img3.png";
+import Img4 from "../../media/img4.png";
 
 const StaticImage = (imageData) => {
-  
+  const images = {    
+    Img1: Img1,
+    Img2: Img2,
+    Img3: Img3,
+    Img4: Img4,
+  };
 
   return (
              <Image
