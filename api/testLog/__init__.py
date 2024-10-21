0f3719bff92b694458570be7c8f42928f2e2f195
johnShmulsky <john.shmulsky@gptchb.org>
2024-10-21 13:28:22 -0600

    Update __init__.py

--- api/testLog/__init__.py
+++ api/testLog/__init__.py
@@ -1,25 +1,12 @@
-import os
+
 import azure.functions as func
-from typing import List, MutableMapping
-from AppUtils import AuthUtils
-from azure.monitor.ingestion.aio import LogsIngestionClient
 
-
-async def sendLogs():
-    endpoint = os.environ["DATA_COLLECTION_ENDPOINT"]
-    rule_id = os.environ["LOGS_DCR_RULE_ID"]
-    body: List[MutableMapping[str, str]] = [
-        {"EventType": "AccessTableau", "UserID": "12345678sdfsdfed-sb", "ObjectID":"GreatPlainsDemographic", "Message":"Tableau Report displayed to user"}
-    ]
-    credential = AuthUtils.getCredential()
-    client = LogsIngestionClient(endpoint=endpoint, credential=credential, logging_enable=True)
-    async with client:
-        await client.upload(rule_id=rule_id, stream_name=os.environ["LOGS_DCR_STREAM_NAME"], logs=body)
+from AppUtils import LogUtils
 
 
 async def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
-        await sendLogs()
+        await LogUtils.sendLogs(req,'logTest','','testing function')
     except Exception as ex:
         return func.HttpResponse(ex.message)  
     return func.HttpResponse("success")
