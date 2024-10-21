26f35400e753e40273354cdc50189c1cddfa885f
johnShmulsky <john.shmulsky@gptchb.org>
2024-10-21 12:07:42 -0600

    Update __init__.py

--- api/testLog/__init__.py
+++ api/testLog/__init__.py
@@ -9,7 +9,7 @@ async def sendLogs():
     endpoint = os.environ["DATA_COLLECTION_ENDPOINT"]
     rule_id = os.environ["LOGS_DCR_RULE_ID"]
     body: List[MutableMapping[str, str]] = [
-        {"TimeGenerated": "2024-10-17 23:51:14", "EventType": "AccessTableau", "UserID": "12345678sdfsdfed-sb", "ObjectID":"GreatPlainsDemographic", "Message":"Tableau Report displayed to user"}
+        {"EventType": "AccessTableau", "UserID": "12345678sdfsdfed-sb", "ObjectID":"GreatPlainsDemographic", "Message":"Tableau Report displayed to user"}
     ]
     credential = AuthUtils.getCredential()
     client = LogsIngestionClient(endpoint=endpoint, credential=credential, logging_enable=True)
