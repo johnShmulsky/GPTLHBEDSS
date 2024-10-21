bb9ac8a99b3bf20d47e88aea6cf635e2cc2aaf6c
johnShmulsky <john.shmulsky@gptchb.org>
2024-10-21 13:44:25 -0600

    Update LogUtils.py

--- api/AppUtils/LogUtils.py
+++ api/AppUtils/LogUtils.py
@@ -4,13 +4,16 @@ from AppUtils import AuthUtils
 from azure.monitor.ingestion.aio import LogsIngestionClient
 
 
-async def sendLogs(req,eventType,objectID, message):
+async def sendLogs(userDetails,eventType,objectID, message):
     endpoint = os.environ["DATA_COLLECTION_ENDPOINT"]
     rule_id = os.environ["LOGS_DCR_RULE_ID"]
     body: List[MutableMapping[str, str]] = [
-        {"EventType": eventType, "UserID":AuthUtils.getPrincipal(req)['userDetails'], "ObjectID":objectID, "Message":message}
+        {"EventType": eventType, "UserID":userDetails, "ObjectID":objectID, "Message":message}
     ]
     credential = AuthUtils.getCredential()
     client = LogsIngestionClient(endpoint=endpoint, credential=credential, logging_enable=True)
     async with client:
         await client.upload(rule_id=rule_id, stream_name=os.environ["LOGS_DCR_STREAM_NAME"], logs=body)
+
+async def logEvent(req,eventType,objectID, message):
+    await sendLogs(AuthUtils.getPrincipal(req)['userDetails'],eventType,objectID, message)
