69e113ecc3949b4ce2bbd7e25ff0b81722b3d792
johnShmulsky <john.shmulsky@gptchb.org>
2024-10-22 08:30:36 -0600

    Update __init__.py

--- api/listBlobs/__init__.py
+++ api/listBlobs/__init__.py
@@ -5,15 +5,18 @@ import logging
 
 
 async def main(req: func.HttpRequest) -> func.HttpResponse:
+    container = req.params.get('container')
+    principal = AuthUtils.getPrincipal(req)
     try:
         return_json = []
-        container = req.params.get('container')
-        principal = AuthUtils.getPrincipal(req)
         SecurityUtils.canAccessDirectory(container, principal['userRoles'])
         blobNames = await BlobUtils.listBlobs(container)
         for blobName in blobNames:
             return_json.append({'container':container ,'blob':blobName})
         await LogUtils.sendLogs(principal['userDetails'],'ListBlobs',container,'User accessed directory {0}'.format(container))
         return func.HttpResponse(json.dumps(return_json,indent=4))
+    except AssertionError:
+        LogUtils.sendLogs(principal['userDetails'],'UnauthoriedAccess',container,'Unauthorized access attempt made on directory')
+        return func.HttpResponse('Unauthorized', status_code=403)
     except Exception as e:
         return func.HttpResponse(e.message)
