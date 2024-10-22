a42db81bd575018b30209cac2e07f3f73afff43e
johnShmulsky <john.shmulsky@gptchb.org>
2024-10-22 08:23:24 -0600

    Update __init__.py

--- api/listBlobs/__init__.py
+++ api/listBlobs/__init__.py
@@ -1,4 +1,4 @@
-from AppUtils import BlobUtils,LogUtils
+from AppUtils import BlobUtils,LogUtils,SecurityUtils,AuthUtils
 import azure.functions as func
 import json
 import logging
@@ -8,10 +8,12 @@ async def main(req: func.HttpRequest) -> func.HttpResponse:
     try:
         return_json = []
         container = req.params.get('container')
+        principal = AuthUtils.getPrincipal(req)
+        SecurityUtils.canAccessDirectory(container, principal['userRoles'])
         blobNames = await BlobUtils.listBlobs(container)
         for blobName in blobNames:
             return_json.append({'container':container ,'blob':blobName})
-        await LogUtils.logEvent(req,'ListBlobs',container,'User accessed directory {0}'.format(container))
+        await LogUtils.sendLogs(principal['userDetails'],'ListBlobs',container,'User accessed directory {0}'.format(container))
         return func.HttpResponse(json.dumps(return_json,indent=4))
     except Exception as e:
         return func.HttpResponse(e.message)
